e429b5b98d99da485c2fe8410794ab92a40d2d00
khw7385 <khw7385@naver.com>
2025-12-26 16:27:07 +0900

    feat: bastion host 에 대한 모듈 정의

---
+++ terraform/modules/bastion/outputs.tf
@@ -0,0 +1,14 @@
+output "bastion_public_ip" {
+  description = "Bastion Host Public IP"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "bastion_security_group_id" {
+  description = "Bastion Host Security Group ID"
+  value       = aws_security_group.bastion_sg.id
+}
+
+output "private_key_path" {
+  description = "접속용 Private Key 파일 경로"
+  value       = local_file.private_key_file.filename
+}
